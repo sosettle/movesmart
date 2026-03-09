909cb1b6043a5ef177349e82d0121e2b4bdd2c39
Divya Andem <divyaand@umich.edu>
2026-03-09 00:18:50 +0000

    basic sliders

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 # streamlit app
+import streamlit as st
+import numpy as np
+
+# Feature names
+features = ["Affordability", "Safety", "Job Opportunities", "Health"]
+
+st.title("MoveSmart: City Recommendation")
+
+# User input sliders
+st.header("Set your preferences")
+
+user_weights = []
+for feature in features:
+    w = st.slider(feature, 0.0, 1.0, 0.5)  # min=0, max=1, default=0.5
+    user_weights.append(w)
